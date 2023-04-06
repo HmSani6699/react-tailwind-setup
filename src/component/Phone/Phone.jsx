b9d873d5994520a1d3a3d77a2ce3cdde2d3d1f15
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-06 16:14:10 +0600

    ad to phone part

--- src/component/Phone/Phone.jsx
+++ src/component/Phone/Phone.jsx
@@ -1,12 +1,25 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const Phone = () => {
     const [phones, setPhones] = useState([]);
-    useEffect(() => {
-        fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-            .then(res => res.json())
-            .then(data => console.log(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // }, [])
+
+
+    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+        .then(data => {
+            const loadData = data.data.data;
+            const prosseing = loadData.map(phone => {
+                const slug = phone.slug.split('-');
+                const price = slug[1];
+                const newPhone = {
+                }
+            })
+        })
     return (
         <div>
 
