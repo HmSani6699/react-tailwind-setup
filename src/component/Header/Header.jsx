e8d0f269517cd6a8097cc30c996fd54a25b96448
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-06 14:36:53 +0600

    ad to togol menubar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
-import { Bars3Icon } from '@heroicons/react/24/solid';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className=' bg-orange-300 py-4 px-4'>
-            <div className='md:hidden'>
-                <Bars3Icon className="h-6 w-6 text-black-500" />
+        <div className=' bg-orange-300'>
+            <div onClick={() => setOpen(!open)} className='md:hidden py-4 px-4'>
+                <span>{open ? <XMarkIcon className="h-6 w-6 text-black-500" />
+                    : <Bars3Icon className="h-6 w-6 text-black-500" />}
+                </span>
             </div>
-            <nav className=''>
-                <ul className=' md:flex'>
+            <nav className='relative bg-orange-300'>
+                <ul className={`md:flex duration-500 md:static absolute ${open ? "top-0" : "-top-60"} bg-orange-300 px-4 py-2`}>
                     <li><a href="/">Home</a></li>
                     <li><a href="/">About</a></li>
                     <li><a href="/">Contact</a></li>
